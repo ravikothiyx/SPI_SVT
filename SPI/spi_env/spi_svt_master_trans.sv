6b7543467ee2653aaa3cbc113d221702e1156236
Er-Harekrishna <hkray2712@gmail.com>
2023-10-06 15:46:09 +0530

    spi_svt_master_trans added

---
+++ SPI/spi_env/spi_svt_master_trans.sv
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////// 
+// File:          spi_svt_master_agent.sv
+// Version:       v1
+// Developer:     Harekrishna
+// Project Name:  SPI
+// Discription:
+/////////////////////////////////////////////////
+
+
+//
+// Class Description:
+//
+//
+`ifndef SPI_SVT_MASTER_TRANS_SV
+`define SPI_SVT_MASTER_TRANS_SV
+
+class spi_svt_master_trans extends uvm_sequence_item;
+
+endclass
+`endif 
