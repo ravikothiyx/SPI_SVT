92d2b55056ced89c40e3897ae9ef17b03623cc13
RAVI KOTHIYA <kothiyaravi7@gmail.com>
2023-10-19 11:18:34 +0530

    Commiting Master coverage, slave coverage & Master monitor files

--- SPI/master_agent/spi_uvc_master_monitor.sv
+++ SPI/master_agent/spi_uvc_master_monitor.sv
@@ -13,9 +13,23 @@ class spi_uvc_master_monitor extends uvm_monitor;
    
    /** UVM Factory Registration Macro**/
    `uvm_component_utils(spi_uvc_master_monitor);
+   
+   /**Declaration for queue array*/
+   bit que_mosi[$];
+   bit que_miso[$];
+
+   /**Declaration for temporary variable*/
+   bit temp_mosi;
+   bit temp_miso;
 
    /** Spi interface instance */
    virtual spi_uvc_if vif;
+
+   /**Sequence item Handle*/
+   spi_uvc_transaction trans_h;
+
+   /** Instance of Register config*/
+   spi_uvc_reg_cfg reg_cfg_h;
 
    /** Analysis port for scoreaboard and coverage collector*/
    uvm_analysis_port#(spi_uvc_transaction) item_collected_port;
@@ -39,12 +53,15 @@ endclass : spi_uvc_master_monitor
    function spi_uvc_master_monitor::new(string name = "spi_uvc_master_monitor",uvm_component parent);
       super.new(name,parent);
       item_collected_port = new("item_collected_port",this);
+      trans_h = new();
    endfunction : new
 
    /** Build_phase*/
    function void spi_uvc_master_monitor::build_phase(uvm_phase phase);
       super.build_phase(phase);
       `uvm_info(get_type_name(),"START OF BUILD_PHASE",UVM_HIGH);
+      if(!uvm_config_db#(spi_uvc_reg_cfg)::get(this,"","reg_cfg_h",reg_cfg_h))
+        `uvm_fatal(get_full_name(),"Not able to get Register configuration class");
       `uvm_info(get_name(),"INSIDE BUILD_PHASE",UVM_DEBUG);
       `uvm_info(get_type_name(),"END OF BUILD_PHASE",UVM_HIGH);
    endfunction : build_phase
@@ -59,7 +76,88 @@ endclass : spi_uvc_master_monitor
    
    /** Run_phase*/
    task spi_uvc_master_monitor::run_phase(uvm_phase phase);
-      `uvm_info(get_type_name(),"START OF RUN_PHASE",UVM_HIGH);
+      `uvm_info(get_type_name(),"START OF RUN_PHASE",UVM_HIGH);      
+      
+      forever begin
+      
+      wait(!vif.rstn);
+      @(vif.sclk)
+      
+      temp_mosi = vif.mosi;
+      que_mosi.push_back(temp_mosi);
+
+      temp_miso= vif.miso;
+      que_miso.push_back(temp_miso);
+
+      /**Condition:Phase & Polarity both High */
+      if(reg_cfg_h.SPICR1[2])begin
+        if(reg_cfg_h.SPICR1[3])begin
+          @(posedge vif.sclk)
+          for(int i=0;i<=7;i++)
+          trans_h.header[i]=que_mosi.pop_front();
+
+            /**Check for Write & Read operation, Accordingly we will monitor "wr_data & rd_data"*/
+            if(trans_h.header[1])begin
+              for(int i=0;i<=7;i++)
+              trans_h.wr_data[i]=que_mosi.pop_front();
+            end
+            else begin
+              for(int i=0;i<=7;i++)
+              trans_h.rd_data[i]=que_miso.pop_front();
+            end
+        end
+        /**Condition:Phase = 1 & Polarity = 0*/
+        else begin
+          @(negedge vif.sclk)
+          for(int i=0;i<=7;i++)
+          trans_h.header[i]=que_mosi.pop_front();
+            /**Check for Write & Read operation, Accordingly we will monitor "wr_data & rd_data"*/
+            if(trans_h.header[1])begin
+              for(int i=0;i<=7;i++)
+              trans_h.wr_data[i]=que_mosi.pop_front();
+            end
+            else begin
+              for(int i=0;i<=7;i++)
+              trans_h.rd_data[i]=que_miso.pop_front();
+            end
+        end
+      end
+      /**Condition:Phase = 0 & Polarity = 0*/
+      else begin
+        if(!reg_cfg_h.SPICR1[3])begin
+          @(posedge vif.sclk)
+          for(int i=0;i<=7;i++)
+          trans_h.header[i]=que_mosi.pop_front();
+            /**Check for Write & Read operation from the first bit of "header"*/
+            /**Accordingly we will monitor "wr_data & rd_data"*/
+            if(trans_h.header[1])begin
+              for(int i=0;i<=7;i++)
+              trans_h.wr_data[i]=que_mosi.pop_front();
+            end
+            else begin
+              for(int i=0;i<=7;i++)
+              trans_h.rd_data[i]=que_miso.pop_front();
+            end
+        end
+        /**Condition:Phase = 0 & Polarity = 0*/
+        else begin
+          @(negedge vif.sclk)
+          for(int i=0;i<=7;i++)
+          trans_h.header[i]=que_mosi.pop_front();
+            /**Check for Write & Read operation from the first bit of "header"*/
+            /**Accordingly we will monitor "wr_data & rd_data"*/
+            if(trans_h.header[1])begin
+              for(int i=0;i<=7;i++)
+              trans_h.wr_data[i]=que_mosi.pop_front();
+            end
+            else begin
+              for(int i=0;i<=7;i++)
+              trans_h.rd_data[i]=que_miso.pop_front();
+            end
+        end
+      end
+      item_collected_port.write(trans_h);
+      end
       `uvm_info(get_type_name(),"INSIDE RUN_PHASE",UVM_DEBUG);
       `uvm_info(get_type_name(),"END OF RUN_PHASE",UVM_HIGH);
    endtask : run_phase
